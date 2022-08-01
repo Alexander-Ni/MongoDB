b7f40b69c465a7d9fb76c4fc4b0b94b9f6f63731
Alexander Nielsen <nielsen.alexander1@gmail.com>
2022-08-01 17:46:14 +0200

    List movies on client

--- client/index.jsx
+++ client/index.jsx
@@ -1,38 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Router, Routes} from "react-router-dom";
-
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 
 function FrontPage() {
-    return <div>
-        <h1>MOVIES</h1>
-        <ul>
-            <li><Link to={"/movies"}>List movies</Link></li>
-            <li><Link to={"/movies/new"}>Add new movie</Link></li>
-        </ul>
-    </div>;
+  return (
+    <div>
+      <h1>MOVIES</h1>
+      <ul>
+        <li>
+          <Link to={"/movies"}>List movies</Link>
+        </li>
+        <li>
+          <Link to={"/movies/new"}>Add new movie</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function useLoading(loadingFunction) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  async function load() {
+    try {
+      setLoading(true);
+      setData(await loadingFunction());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+  return { loading, error, data };
+}
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error("Failed to load " + res.status + ": " + res.statusText);
+  }
+  return await res.json();
 }
 
 function ListMovies() {
-    return <div>
-        <h1>Movies in the database</h1>
-    </div>;
+  const { loading, error, data } = useLoading(async () =>
+    fetchJSON("/api/movies")
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Movies in the database</h1>
+
+      <ul>
+        {data.map((movie) => (
+          <li key={movie.title}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function AddNewMovie() {
-    return <form>
-        <h1>Add new movie</h1>
-    </form>;
+  return (
+    <form>
+      <h1>Add new movie</h1>
+    </form>
+  );
 }
 
 function Application() {
-    return <BrowserRouter>
-        <Routes>
-            <Route path={"/"} element={<FrontPage />} />
-            <Route path={"/movies"} element={<ListMovies />} />
-            <Route path={"/movies/new"} element={<AddNewMovie />} />
-        </Routes>
-    </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/movies"} element={<ListMovies />} />
+        <Route path={"/movies/new"} element={<AddNewMovie />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(<Application />, document.getElementById("app"));
