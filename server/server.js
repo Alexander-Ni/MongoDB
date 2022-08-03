7b733ec9566c3dde0af339ff099129b635e8de41
Alexander Nielsen <nielsen.alexander1@gmail.com>
2022-08-03 15:21:01 +0200

    Connect server to MongoDB

--- server/server.js
+++ server/server.js
@@ -24,6 +24,6 @@ app.use((req, res, next) => {
   }
 });
 
-const server = app.listen(process.env.PORT || 8080, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
 });
